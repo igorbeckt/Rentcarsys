64b7e969864b4197840a4a62c6c68f9c730d1eec
Igor Nascimento <igorbeckt@hotmail.com>
2023-04-24 13:58:19 -0300

    Adicionando cliente
    Model, viewmodels, controller

---
+++ RentCarSys/Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Localdorateste.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Localdorateste.Data
+{
+    public class Contexto : DbContext
+
+    {
+
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
+
