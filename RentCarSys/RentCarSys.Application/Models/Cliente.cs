72068020b847c417c1cc5d752c37856dd798e393
Igor Nascimento <97426648+igorbeckt@users.noreply.github.com>
2023-08-12 00:31:57 -0300

    remove repor back-end
    remove repor back-end

---
+++ RentCarSys/RentCarSys.Application/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarSys.Enums;
+
+namespace Localdorateste.Models
+{    
+    public class Cliente
+    {        
+        public int Id { get; set; }
+        public ClienteStatus Status { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+        public long RG { get; set; }
+        public long CPF { get; set; }
+
+        public int? ReservaId { get; set; }
+        public virtual Reserva? Reserva { get; set; }
+    }
+}
---
+++ RentCarSys/RentCarSys.Test/UnitTest/ServicesTest/ClienteControllerTest.cs
@@ -0,0 +1,11 @@
+using Localdorateste.Data;
+using Localdorateste.Models;
+using Localdorateste.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace RentCarSys.Test.UnitTest.Controllers
+{
+
+}
